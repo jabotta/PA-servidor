97f70956dc6b1e963f838c2703e8133cf3a2d863
Andres Botta <a.botta@case-inc.com>
2014-08-25 17:03:46 -0300

    controlador usuario

--- src/src/Controlador/Clases/ControladorUsuarios.java
+++ src/src/Controlador/Clases/ControladorUsuarios.java
@@ -1,5 +1,60 @@
 package Controlador.Clases;
 
+import Controlador.DataTypes.DataCategoria;
+import Controlador.DataTypes.DataCliente;
+import Controlador.DataTypes.DataOrdenCompra;
+import Controlador.DataTypes.DataProveedor;
+import Controlador.DataTypes.DataUsuario;
+import java.util.ArrayList;
+
 public class ControladorUsuarios implements IControladorUsuarios{
     
+    private Cliente clienteElegido;
+    private Proveedor proveedorElegido;
+//    - prvLst : Set<Proveedor> 
+    
+    public void ingresarDatos(DataUsuario usuario){
+        
+    }
+    
+    public void validarDatosUsuario(){
+        
+    }
+    
+    public void guardarUsuario(){
+        
+    }
+    
+//    public void cancelar(){
+//        
+//    }
+    
+//    public DataTablaUsuario mostrarTablaCliente(){
+//        return null;
+//    }
+    
+    public void elegirCliente(String nickname){
+        
+    }
+    
+    public DataCliente mostrarDatosCliente(){
+        return null;
+    }
+    
+    public ArrayList<DataOrdenCompra> listarOrdenes(){
+        return null;
+    }
+    
+    public ArrayList<DataProveedor> listarProveedores(){
+        return null;
+    }
+    
+    public void elegirProveedor(String nickname){
+        
+    }
+    
+    public DataProveedor mostrarDatosProveedor(){
+        return null;
+    }
+        
 }
--- src/src/Controlador/Clases/IControladorUsuarios.java
+++ src/src/Controlador/Clases/IControladorUsuarios.java
@@ -1,5 +1,23 @@
 package Controlador.Clases;
 
+import Controlador.DataTypes.DataCliente;
+import Controlador.DataTypes.DataOrdenCompra;
+import Controlador.DataTypes.DataProveedor;
+import Controlador.DataTypes.DataUsuario;
+import java.util.ArrayList;
+
 public interface IControladorUsuarios {
+    
+    public void ingresarDatos(DataUsuario usuario);
+    public void validarDatosUsuario();
+    public void guardarUsuario();
+//    public void cancelar();
+//    public DataTablaUsuario mostrarTablaCliente();
+    public void elegirCliente(String nickname);
+    public DataCliente mostrarDatosCliente();
+    public ArrayList<DataOrdenCompra> listarOrdenes();
+    public ArrayList<DataProveedor> listarProveedores();
+    public void elegirProveedor(String nickname);
+    public DataProveedor mostrarDatosProveedor();
     
 }
