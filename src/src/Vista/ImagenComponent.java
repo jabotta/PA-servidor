bad6678ff1385f3bc910c2391035390079162660
Rodrigo Rossi <rodrigo.rossi.trabal@gmail.com>
2014-08-31 10:21:41 -0300

    Implemento parte de la ui

--- src/src/Vista/ImagenComponent.java
+++ src/src/Vista/ImagenComponent.java
@@ -3,41 +3,94 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Vista;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author rodro
  */
-public class ImagenComponent extends JPanel{
+public class ImagenComponent extends JPanel {
+
     private final JLabel nombre;
     private final JButton editBtn;
     private final JButton deleteBtn;
     private File f;
+    private boolean isDeleted;
 
     public ImagenComponent(File f) {
-       
+
         this.f = f;
-        
+        this.setSize(400, 100);
         nombre = new JLabel(f.getName());
+        System.out.println(f.getName());
         editBtn = new JButton("editar");
         deleteBtn = new JButton("Borrar");
-        
-        this.setLayout(null);
-        this.setBounds(0, 0, 300, 50);
+        nombre.setVerticalAlignment(0);
+        nombre.setBounds(10, 0, 100, 30);
+        editBtn.setBounds(90, 10, 80, 20);
+        deleteBtn.setBounds(180, 10, 80, 20);
         this.add(nombre);
         this.add(editBtn);
         this.add(deleteBtn);
+        repaint();
+
+        editBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                editarImagen(e);
+            }
+        });
+        deleteBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                delete(e);
+            }
+
+        });
     }
-    
-    
-    
-    
+
+    private void delete(ActionEvent e) {
+        
+        this.setVisible(false);
+        this.isDeleted = true;
+    }
+
+    private void editarImagen(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregarImgBtnActionPerformed
+
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Imagenes", "jpg", "gif", "png");
+        chooser.setFileFilter(filter);
+        chooser.setDialogTitle("Eliga una imagen para su producto");
+        int returnVal = chooser.showOpenDialog(this.getParent());
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            cambiarArchivo(chooser.getSelectedFile());
+
+        }
+
+    }
+
+    private void cambiarArchivo(File selectedFile) {
+        
+        this.f = selectedFile;
+        this.nombre.setText(f.getName());
+        repaint();
+                
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
 }
---
+++ src/src/Vista/SelectorDeImagenes.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Vector;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author rodro
+ */
+public class SelectorDeImagenes extends JPanel {
+
+    private ArrayList<ImagenComponent> imagenes;
+    private final JPanel pane;
+    private final JPanel paneWrapper;
+
+    public SelectorDeImagenes() {
+        
+        setLayout(new BorderLayout());
+        paneWrapper =  new JPanel();
+        paneWrapper.setLayout(new BorderLayout());
+                
+        add(paneWrapper,BorderLayout.CENTER);
+ 
+        pane =  new JPanel();
+        pane.setLayout(new SpringLayout());
+        
+        pane.setLocation(0, 40);
+        paneWrapper.add(pane,BorderLayout.NORTH);
+        
+        imagenes = new ArrayList();
+        JButton addIMG = new JButton("Agregar Imagen");
+        addIMG.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agregarImgBtnActionPerformed(e);
+            }
+
+        });
+        
+        setSize(400, 1200);
+        addIMG.setBounds(100, 20, 0, 0);
+        add(addIMG,BorderLayout.NORTH);
+        
+        
+        
+
+    }
+
+    private void agregarImgBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agregarImgBtnActionPerformed
+
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Imagenes", "jpg", "gif", "png");
+        chooser.setFileFilter(filter);
+        chooser.setDialogTitle("Eliga una imagen para su producto");
+        int returnVal = chooser.showOpenDialog(this.getParent());
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            addImagenComponent(chooser.getSelectedFile());
+
+        }
+
+    }
+
+    private void addImagenComponent(File f) {
+       
+        ImagenComponent ic = new ImagenComponent(f);
+        imagenes.add(ic);
+        pane.setSize(500,imagenes.size() * 50);
+        pane.repaint();
+        ic.setLocation(10, imagenes.size() * 40);
+        pane.add(ic);
+        SpringUtilities.makeCompactGrid(pane,imagenes.size(),1,0,0,6,6);
+        revalidate();
+        repaint(); 
+        getParent().repaint( );
+
+    }
+}
